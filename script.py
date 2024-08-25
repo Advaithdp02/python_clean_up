d627188912a7ae095d32d24db2e96ada68982a0a
Advaithdinesan02 <advaithdp@email.com>
2024-08-25 21:05:39 +0530

    ADD  DIRECTORY TO BE SORTED INTO

--- script.py
+++ script.py
@@ -5,3 +5,11 @@ ext_vid=['mp4','mkv','avi']
 ext_doc=['pdf','docx','ppt']
 ext_comp=['zip','tar']
 ext_inst=['dmg','iso','exe']
+
+dir_item=['movie','image',"module",'Compression','installation']
+downloads_path = os.path.expanduser('~/Downloads')+'/'
+print(downloads_path)
+files=os.listdir()
+for dir in dir_item:
+    if not os.path.exists(downloads_path+dir):
+        os.mkdir(downloads_path+dir)
