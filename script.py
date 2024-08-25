19f1ee96ba1481bca4dd8f31e71c47f1bc32b9e6
Advaithdinesan02 <advaithdp@email.com>
2024-08-25 22:24:08 +0530

    Sorting the files and moving it using shutil

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 import os
 import collections
 from pprint import pprint
+import shutil
 
-ext_img=['.jpg','.jpeg','.png','Screenshot']
-ext_vid=['.mp4','.mkv','.avi']
-ext_doc=['.pdf','.docx','.ppt']
-ext_comp=['.zip','.tar']
-ext_inst=['.dmg','.iso','.exe']
+ext_img=['jpg','jpeg','png','Screenshot']
+ext_vid=['mp4','mkv','avi']
+ext_doc=['pdf','docx','ppt']
+ext_comp=['zip','tar']
+ext_inst=['dmg','iso','exe']
 
 dir_item=['movie','image',"module",'Compression','installation']
 downloads_path = os.path.expanduser('~/Downloads')+'/'
@@ -23,6 +24,21 @@ for file_name in files:
         file_ext=file_name.split('.')[-1]
         file_mapping[file_ext].append(file_name)
 
+
 for f_ext,f_list in file_mapping.items():
-    print(f_ext)
-    print(f_list)
+    if f_ext in ext_comp:
+        for file in f_list:
+            shutil.move(downloads_path+file , downloads_path+"Compression")
+    if f_ext in ext_img:
+        for file in f_list:
+            shutil.move(downloads_path+file , downloads_path+"image")
+    if f_ext in ext_vid:
+        for file in f_list:
+            shutil.move(downloads_path+file , downloads_path+"movie")
+    if f_ext in ext_doc:
+        for file in f_list:
+            shutil.move(downloads_path+file , downloads_path+"module")
+    if f_ext in ext_inst:
+        for file in f_list:
+            shutil.move(downloads_path+file , downloads_path+"installation")
+    
